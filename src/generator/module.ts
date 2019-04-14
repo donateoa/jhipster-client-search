ad803443be5e10ff52a2bb99eac261d3275ade5e
Alessandro Donateo <alessandro.donateo@gmail.com>
2019-04-14 14:55:11 +0200

    Add TapModule

--- src/generator/module.ts
+++ src/generator/module.ts
@@ -1,6 +1,14 @@
+import {IFlow} from '../model/flow';
+
 var fse = require('fs-extra');
 
-export function tapModule(srcFolder: string) {
-  const file = `${srcFolder}.module.ts`;
-  return fse.pathExists(file).then(exists => console.log(exists))  // => false
+export function tapModule(iflow: IFlow) {
+  const file = `${iflow.destFolder}/${iflow.entity}.module.ts`;
+  return fse.pathExists(file).then((exists: Boolean) => {
+    if (!exists) {
+      throw new Error(`${file} does not exist.`);
+    } else {
+      console.log('exists');
+    }
+  })  // => false
 }
