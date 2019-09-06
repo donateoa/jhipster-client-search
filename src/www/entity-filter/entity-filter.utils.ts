c0bf47ae914f541476374331fea15383ed2e68a0
Alessandro Donateo <alessandro.donateo@gmail.com>
2019-09-06 16:15:29 +0200

    fix entityFilterDecodeCriteria

--- src/www/entity-filter/entity-filter.utils.ts
+++ src/www/entity-filter/entity-filter.utils.ts
@@ -1,40 +1,47 @@
-import { Injectable } from '@angular/core';
-import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
-import { Observable, of } from 'rxjs';
+import {Injectable} from '@angular/core';
+import {ActivatedRouteSnapshot, Resolve, RouterStateSnapshot} from '@angular/router';
+import {Observable, of } from 'rxjs';
 
 function uncodedCriteria(criteria: string) {
-    return criteria.split('##').map(t => {
-        const arr = decodeURIComponent(t).split('=');
-        if (!arr[1]) {
-            return {};
-        }
-        const key = arr[0];
-        const value = arr[1];
-        const out = {};
-        out['key'] = key;
-        out['value'] = value;
-        return out;
-    });
+  return criteria.split('##').map(t => {
+    const arr = decodeURIComponent(t).split('=');
+    if (!arr[1]) {
+      return {};
+    }
+    const key = arr[0];
+    const value = arr[1];
+    const out = {};
+    out['key'] = key;
+    out['value'] = value;
+    return out;
+  });
 }
 function decodeCriteriaItem(myData: any) {
-    const out = [];
-    for (const key in myData) {
-        if (myData.hasOwnProperty(key)) {
-            out.push(encodeURIComponent(myData[key]));
-        }
+  const out = [];
+  for (const key in myData) {
+    if (myData.hasOwnProperty(key)) {
+      out.push(encodeURIComponent(myData[key]));
     }
-    return out.join('=');
+  }
+  return out.join('=');
 }
 export function entityFilterDecodeCriteria(criteria: any[]) {
-    return encodeURIComponent(criteria.map(t => decodeCriteriaItem(t)).join('##'));
+  if (!criteria) {
+    return null;
+  }
+  return encodeURIComponent(
+      criteria.map(t => decodeCriteriaItem(t)).join('##'));
 }
 
-@Injectable({ providedIn: 'root' })
+@Injectable({providedIn: 'root'})
 export class CriteriaParamsResolve implements Resolve<any> {
-    constructor() {}
+  constructor() {}
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
-        const s = route.queryParams['criteria'] ? uncodedCriteria(route.queryParams['criteria']) : null;
-        return of(s);
-    }
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot):
+      Observable<any> {
+    const s = route.queryParams['criteria'] ?
+        uncodedCriteria(route.queryParams['criteria']) :
+        null;
+    return of (s);
+  }
 }
